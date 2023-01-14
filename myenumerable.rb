e1bec0f1d98fabac78cb4902b21311d375e5547e
nunegabriel <nune.gabriel.gn@gmail.com>
2023-01-14 10:25:08 +0000

    ADD enumerable.rb

---
+++ myenumerable.rb
@@ -0,0 +1,17 @@
+module MyEnumerable
+    # all method
+    def all?
+      @list.all? { |item| yield(item) }
+    end
+  
+    # any method
+    def any?
+      @list.any? { |item| yield(item) }
+    end
+  
+    # filter method
+    def filter
+      @list.select { |item| yield(item) }
+    end
+  end
+  
