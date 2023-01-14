60b4684433a5c02dc1629dc38973cb55ddcf0fb4
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-14 11:54:53 +0100

    add filter test

--- list.rb
+++ list.rb
@@ -15,10 +15,3 @@ list1 = MyList.new(1, 2, 3, 4)
 # Test #filter
 puts list1.filter(&:even?)
 
-# Test #any?
-puts(list1.any? { |e| e == 5 })
-puts(list1.any? { |e| e == 2 })
-
-# Test #all?
-puts(list1.all? { |e| e < 5 })
-puts(list1.all? { |e| e > 5 })
